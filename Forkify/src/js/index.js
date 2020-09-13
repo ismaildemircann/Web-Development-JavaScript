3f02dbc42a66b69b22f30ff9c03a56bd2a670250
ismaildemircann <ismaildemircann98@gmail.com>
2020-09-13 16:16:26 +0300

    Added Forkify Api

---
+++ Forkify/src/js/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+async function getResults(query) {
+  try {
+    const res = await axios(
+      `https://forkify-api.herokuapp.com/api/search?&q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults("pizza");
